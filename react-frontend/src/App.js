68fd5cf1df7b44022a59b4e0ad81e13e6ea53893
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-05-31 20:08:53 +0600

    47 - Implementing a Basic Desktop & Mobile Navigation

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,19 +4,23 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Users from './user/pages/Users';
 import { Redirect, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
--- react-frontend/src/user/pages/Users.js
+++ react-frontend/src/user/pages/Users.js
@@ -1,8 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Mr user',
+            image: 'https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg',
+            places: 3,
+        }
+    ];
+
     return (
-        <h2>Users</h2>
+        <UsersList items={USERS} />
     );
 };
 
