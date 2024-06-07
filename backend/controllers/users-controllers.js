1d82f554446c93a5717b6cf8b0bcdcd0ca2113c8
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-06-07 18:19:59 +0600

    9 - Connecting the Backend to the Database MongoDB & Mongoose

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,51 +1,70 @@
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
-const DUMMY_USERS = [
-  {
-    id: 'u1',
-    name: 'M Tester',
-    email: 'test@test.com',
-    password: 'testers'
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, '-password');
+  } catch (err) {
+    return next(new HttpError('Something went wrong', 500))
   }
-];
-
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+  res.json({ users: users.map(user => user.toObject({ getters: true })) });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    return next(new HttpError('Invalid inputs passed, please check your data.', 422));
   }
   const { name, email, password } = req.body;
 
-  const hasUser = DUMMY_USERS.find(u => u.email === email);
-  if (hasUser) {
-    throw new HttpError('Could not create user, email already exists.', 422);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    return next(new HttpError('Signup failed, try again', 500))
   }
 
-  const createdUser = {
-    id: uuidv4(),
+  if (existingUser) {
+    return next(new HttpError('User existed. Login instead', 422));
+  }
+
+  const createdUser = new User({
     name,
     email,
-    password
-  };
+    image: 'https://static.vecteezy.com/system/resources/thumbnails/005/129/844/small_2x/profile-user-icon-isolated-on-white-background-eps10-free-vector.jpg',
+    password,
+    places: [],
+  });
 
-  DUMMY_USERS.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Signup failed, try again',
+      500
+    );
+    return next(error);
+  }
 
-  res.status(201).json({ user: createdUser });
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    return next(new HttpError('Login failed, try again', 500))
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    return next(new HttpError('Invalid credentials, could not log u in', 401));
   }
 
   res.json({ message: 'Logged in!' });
